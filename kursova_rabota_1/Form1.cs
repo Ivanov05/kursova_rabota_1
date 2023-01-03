9656647add557966b5132a9d9b13bf3cc6de63ce
Ivanov05 <ahil71963@gmail.com>
2023-01-03 22:08:19 +0200

    s for cikal se otpechatvat pravougalnici na sluchaino mqsto

--- kursova_rabota_1/Form1.cs
+++ kursova_rabota_1/Form1.cs
@@ -14,6 +14,19 @@ namespace kursova_rabota_1
         }
         private void Rectangle()
         {
-        }
+            for (int i = 0; i < 50; i--)
+            {
+                Random rnd = new Random();
+
+                int width = rnd.Next(50, 200);
+                int height = rnd.Next(50, 200);
+                int x = rnd.Next(1, this.Width);
+                int y = rnd.Next(1, this.Height);
+
+                Graphics g = this.CreateGraphics();
+                g.DrawRectangle(Pens.Black, new Rectangle(x, y, width, height));
+                Thread.Sleep(3000);
+            }
+         }
     }
 }
